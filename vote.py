66b31a7c6143f45418f1812bcaead8a692e2a491
Franciszka Piasecka <franciszka.piasecka@acaisoft.com>
2018-08-26 11:40:22 +0200

    refactor

--- vote.py
+++ vote.py
@@ -1,58 +1,51 @@
 import argparse
-import random
+import logging
+import sys
 
 import requests
 
-headers = {
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,pl;q=0.7',
-    'content-type': 'application/json;charset=UTF-8',
-    'accept': 'application/json, text/plain',
-    'cache-control': 'no-cache, no-store',
-    'authority': 'app2.sli.do'
-}
+logger = logging.getLogger(__name__)
 
 
-def get_event_uuid(event_tag):
+def get_event_ids(event_tag):
     url = f'https://app2.sli.do/api/v0.5/events?hash={event_tag}'
-    re = requests.get(url, headers=headers)
+    re = requests.get(url)
     d = re.json()[0]
     return d['uuid'], d['event_id']
 
 
 def authenticate(event_uuid):
     url = f'https://app2.sli.do/api/v0.5/events/{event_uuid}/auth'
-    re = requests.post(url, json={}, headers=headers)
-    print(re, re.text)
+    re = requests.post(url, json={})
+    logger.info(f'{re.status_code}, {re.text}')
     token = re.json()['access_token']
     return f'Bearer {token}'
 
 
 def vote(event_id, question_id, event_uuid):
     url = f'https://app2.sli.do/api/v0.5/events/{event_id}/questions/{question_id}/like'
-    print('authenticate')
     auth = authenticate(event_uuid)
-    print('vote')
     re = requests.post(url, json={
         'score': 1
     }, headers={
         'Authorization': auth,
-        **headers
     })
-    print(re, re.text)
+    logger.info(f'{re.status_code}, {re.text}')
     return re.json()['event_question_score']
 
 
-if __name__ == '__main__':
+def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('event_tag', type=str)
     parser.add_argument('question_id', type=int)
     parser.add_argument('votes', type=int)
     args = parser.parse_args()
-    print('get_uuid')
-    event_uuid, event_id = get_event_uuid(args.event_tag)
-    print(event_uuid)
-    random.seed()
+    event_uuid, event_id = get_event_ids(args.event_tag)
     for i in range(args.votes):
-        print(f'vote #{i}')
+        logger.info(f'vote #{i}')
         vote(event_id, args.question_id, event_uuid)
+
+
+if __name__ == '__main__':
+    main()
